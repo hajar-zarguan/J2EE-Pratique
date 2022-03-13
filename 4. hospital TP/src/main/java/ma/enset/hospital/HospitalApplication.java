00d922eea5a4d19f9085066c4cd1e5facf098daa
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-03-13 12:18:55 +0100

    Spring Data

--- 4. hospital TP/src/main/java/ma/enset/hospital/HospitalApplication.java
+++ 4. hospital TP/src/main/java/ma/enset/hospital/HospitalApplication.java
@@ -1,13 +1,65 @@
 package ma.enset.hospital;
-
+import ma.enset.hospital.entities.*;
+import ma.enset.hospital.repositories.ConsultationRepository;
+import ma.enset.hospital.repositories.MedecinRepository;
+import ma.enset.hospital.repositories.PatientRepository;
+import ma.enset.hospital.repositories.RendezVousRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+import java.util.stream.Stream;
+
+
 @SpringBootApplication
-public class HospitalApplication {
+public class HospitalApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalApplication.class, args);
 	}
+	CommandLineRunner start(PatientRepository patientRepository, MedecinRepository medecinRepository, RendezVousRepository rendezVousRepository , ConsultationRepository consultationRepository){
+		return  args -> {
 
+			patientRepository.save(new Patient( null,"", false, new Date(), null, null));
+			Stream.of("hajar","yassin","salah").forEach(name-> {
+			Patient patient = new Patient() ;
+			patient.setNom(name);
+			patient.setMalade(false);
+			patient.setDateNaissance(new Date());
+			patient.setEmail("em");
+			patientRepository.save(patient);
+
+			});
+
+			Stream.of("Issam","Abdeljalil","hafsa").forEach(name-> {
+				Medecin medecin = new Medecin() ;
+				medecin.setNom(name);
+				medecin.setEmail("em");
+				medecin.setSpecialiste("cardiologue");
+				medecinRepository.save(medecin);
+
+			});
+			Patient patient = patientRepository.findByNom("salah");
+			Patient patient1 = patientRepository.findById(1L).orElse(null);
+			Medecin medecin = medecinRepository.findByNom("hafsa");
+			RendezVous rendezVous = new RendezVous() ;
+			rendezVous.setDateRendezVous(new Date());
+			rendezVous.setAnnulee(false);
+			rendezVous.setMedecin(medecin);
+			rendezVous.setPatient(patient1);
+			rendezVous.setStatusRDV(StatutRDV.PENDING);
+			rendezVousRepository.save(rendezVous);
+			RendezVous rendezVous1 = rendezVousRepository.findById(1L).orElse(null);
+			Consultation consultation = new Consultation();
+			consultation.setRapportConsultation("R");
+			consultation.setRendezVous(rendezVous1);
+			consultation.setPrixConsultation(230.78);
+			consultation.setDateConsultation(new Date());
+			consultationRepository.save(consultation);
+			rendezVous.setConsultation(consultation);
+
+		};
+
+	}
 }
--- 4. hospital TP/src/main/java/ma/enset/hospital/entities/RendezVous.java
+++ 4. hospital TP/src/main/java/ma/enset/hospital/entities/RendezVous.java
@@ -1,4 +1,32 @@
 package ma.enset.hospital.entities;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class RendezVous {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long Id ;
+    private Date dateRendezVous ;
+
+    @Enumerated(EnumType.STRING)
+    private  StatutRDV statusRDV ;
+    private  boolean annulee;
+
+    @ManyToOne
+    private  Patient patient ;
+
+    @ManyToOne
+    private  Medecin medecin ;
+
+    @OneToOne
+    private Consultation consultation ;
+
+
 }
